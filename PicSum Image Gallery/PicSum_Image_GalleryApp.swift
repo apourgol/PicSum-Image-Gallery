68e9cb6e07fb990bbfc59e0b51e4b9bf99b05997
Amin Pourgol <amin.pourgol@nuance.com>
2023-09-17 21:04:38 -0400

    Initial Commit

---
+++ PicSum Image Gallery/PicSum_Image_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicSum_Image_GalleryApp.swift
+//  PicSum Image Gallery
+//
+//  Created by Amin Pourgol on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PicSum_Image_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
